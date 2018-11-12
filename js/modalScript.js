8e0b40b4073555b7b60b15fe934176f909fc743a
myexistance <joonyoonlives@gmail.com>
2018-11-12 19:39:46 +1300

    final with the modal

---
+++ js/modalScript.js
@@ -0,0 +1,131 @@
+// MODAL ARABICA
+var modal = document.getElementById("modal_arabica");
+var modalClick = document.getElementById("arabicaGet");
+var span = document.getElementsByClassName("close")[0];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//MODAL ARO CAFE
+var modal = document.getElementById("modal__aroCafe");
+var modalClick = document.getElementById("aroGet");
+var span = document.getElementsByClassName("close")[1];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//ASTORIA
+var modal = document.getElementById("modal__astoria");
+var modalClick = document.getElementById("astoriaGet");
+var span = document.getElementsByClassName("close")[2];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//BLACK COFFEE
+var modal = document.getElementById("modal__blackCoffee");
+var modalClick = document.getElementById("blackGet");
+var span = document.getElementsByClassName("close")[3];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//COLUMBUS
+var modal = document.getElementById("modal__columbus");
+var modalClick = document.getElementById("columbusGet");
+var span = document.getElementsByClassName("close")[4];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//PANAMA
+var modal = document.getElementById("modal__panama");
+var modalClick = document.getElementById("panamaGet");
+var span = document.getElementsByClassName("close")[5];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//FIDELS
+var modal = document.getElementById("modal__fidel");
+var modalClick = document.getElementById("fidelsGet");
+var span = document.getElementsByClassName("close")[6];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//MOJO LAMBTON
+var modal = document.getElementById("modal__mojoLambton");
+var modalClick = document.getElementById("mojoLambGet");
+var span = document.getElementsByClassName("close")[7];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//MOJO TCE
+var modal = document.getElementById("modal__mojoTce");
+var modalClick = document.getElementById("mojoTceGet");
+var span = document.getElementsByClassName("close")[8];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//MOJO WATERFRONT
+var modal = document.getElementById("modal__mojoWater");
+var modalClick = document.getElementById("mojoWaterGet");
+var span = document.getElementsByClassName("close")[9];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//SALVATION
+var modal = document.getElementById("modal__salvation");
+var modalClick = document.getElementById("salvationGet");
+var span = document.getElementsByClassName("close")[10];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//THE HANGER
+var modal = document.getElementById("modal__theHanger");
+var modalClick = document.getElementById("hangerGet");
+var span = document.getElementsByClassName("close")[11];
+modalClick.onclick = function(){
+	modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
